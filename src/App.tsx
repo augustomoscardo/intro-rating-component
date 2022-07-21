69649271bdafc3189d1711942c880703f912a048
Augusto Moscardo <augustomsaluz@gmail.com>
2022-07-21 14:57:11 -0300

    Add Footer

--- src/App.tsx
+++ src/App.tsx
@@ -13,11 +13,37 @@ function App() {
 
   return (
     <RatingProvider>
-      <div className="sm:max-w-[375px] max-w-[1440px] p-4 mx-auto my-0 h-screen flex justify-center items-center">
+      <div className="sm:max-w-[375px] max-w-[1440px] p-4 mx-auto my-0 h-screen flex justify-center items-center flex-col">
         {isSubmitted ? <ThankYou /> : <Rate handleSubmit={handleSubmit} />}
+
+        <div className="flex gap-4 mt-10">
+          <p className="text-gray-300">
+            Challenge by{" "}
+            <a
+              href="https://www.frontendmentor.io/challenges/interactive-rating-component-koxpeBUmI"
+              target="_blank"
+              rel="noreferrer"
+              className="text-orange-800 hover:text-gray-100 font-bold"
+            >
+              Frontend Mentor
+            </a>
+            .
+          </p>
+          <p className="text-gray-300">
+            Coded by{" "}
+            <a
+              href="https://github.com/augustomoscardo"
+              target="_blank"
+              rel="noreferrer"
+              className="text-orange-800 hover:text-gray-100 font-bold"
+            >
+              Augusto Moscardo
+            </a>
+            .
+          </p>
+        </div>
       </div>
       <Toaster />
-      <div></div>
     </RatingProvider>
   );
 }
